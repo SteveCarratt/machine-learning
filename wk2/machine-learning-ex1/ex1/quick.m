f5d9ed0098427aaf622b68789f7f69c022f808a7
Steve Carratt <Steve@cloudhouse.local>
2017-02-08 10:33:00 +0000

    init

---
+++ wk2/machine-learning-ex1/ex1/quick.m
@@ -0,0 +1,12 @@
+X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
+theta = zeros(2, 1); % initialize fitting parameters
+
+% Some gradient descent settings
+iterations = 1500;
+alpha = 0.01;
+
+% compute and display initial cost
+computeCost(X, y, theta)
+
+% run gradient descent
+theta = gradientDescent(X, y, theta, alpha, iterations);
